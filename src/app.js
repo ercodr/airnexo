528da396b030eb2171adb38630d12fccbe86f2ea
ercodr <82173838+Dahcodingkid@users.noreply.github.com>
2022-01-04 08:51:21 -0800

    theme

--- src/app.js
+++ src/app.js
@@ -1,7 +1,43 @@
-const darkmode = document.getElementById('html');
-const dark_mode_toggler = document.getElementById('dark-mode-toggler');
+var darkmode = document.getElementById('html');
+var themeToggler = document.getElementById('toggle-btn');
+var icon = document.getElementById('icon');
 
-dark_mode_toggler.addEventListener('click', () => {
-    darkmode.classList.toggle('dark');
-    // alert('1')
+getTheme();
+iconTheme();
+
+if(getTheme() != 'dark' && getTheme() != 'light'){
+    localStorage.setItem('theme', 'dark');
+    darkmode.classList.add(localStorage.getItem('theme'));
+    iconTheme();
+}
+
+themeToggler.addEventListener('click', () => {
+
+    if(getTheme() == 'light'){
+        darkmode.classList.remove('light');
+        localStorage.setItem('theme', 'dark');
+        darkmode.classList.add(getTheme());
+        iconTheme();
+    } else {
+        darkmode.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+        darkmode.classList.add(getTheme());
+        icon.classList.toggle('text-slate-900');
+        iconTheme();
+    }
 });
+
+function getTheme(){
+    return  String(localStorage.getItem('theme'));
+}
+
+
+function iconTheme(){
+    if(getTheme() == 'dark'){
+        themeToggler.classList.remove('text-slate-900');
+    } else {
+        themeToggler.classList.add('text-slate-900');
+    }    
+}
+
+darkmode.classList.add(getTheme());
